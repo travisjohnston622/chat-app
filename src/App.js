2760414bb0f0f44c6da09c48e2ad6b185e6e0822
Travis Johnston <travisjohnston4588@icloud.com>
2020-03-12 15:58:32 -0500

    file structure updated & app.js configured

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Message from './components/Message';
+import MessageList from './components/MessageList';
+import NewRoomForm from './components/NewRoomForm';
+import RoomList from './components/RoomList';
+import SendMessageForm from './components/SendMessageForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className='app'>
+				<MessageList />
+				<NewRoomForm />
+				<RoomList />
+				<SendMessageForm />
+			</div>
+		);
+	}
 }
 
 export default App;
